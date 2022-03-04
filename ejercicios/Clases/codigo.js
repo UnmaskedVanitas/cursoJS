e45092d925ae3651326bff60f0ed2bdf5642e84c
Jonatan Villarroel <lml@live.cl>
2022-03-04 00:32:22 -0300

    Avance de progreso

---
+++ ejercicios/Clases/codigo.js
@@ -0,0 +1,12 @@
+
+class animal {
+    constructor(especie,edad,color) {
+        this.especie = especie
+        this.edad = edad
+        this.color = color
+    }
+}
+
+const perro = new animal()
+
+document.write(perro)
