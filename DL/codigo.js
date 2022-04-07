f13056f37468f00fde21e956dda8dd7c078565d0
Jonatan Villarroel <lml@live.cl>
2022-04-07 19:30:27 -0400

    Avance de Ejercicios de Clases

---
+++ DL/codigo.js
@@ -0,0 +1,32 @@
+// function saludar(nombre){
+// const frase = `Hola ${nombre}!, como estas?` ;
+// document.write(frase)
+// }
+
+// saludar("Italo") 
+
+const saludar = (nombre)=>{
+    let frase = `Hola ${nombre}! Como estas?`;
+        document.write(frase)
+}
+
+saludar("Andrea")
+saludar("dyland")
+
+let frase = `Hola ${nombre}! Como estas?`;
+
+function saludar(nombre){ //ups
+    let frase = `Hola ${nombre}! Como estas?`;
+    document.write(frase)
+}
+
+const saludar = function(nombre) {
+    let frase = `Hola ${nombre}! Como estas?`;
+    document.write(frase)
+}
+
+const saludar = nombre => {
+    document.write(frase)
+}
+
+saludar("Andrea")
