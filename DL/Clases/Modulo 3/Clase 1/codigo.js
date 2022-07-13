1ac852ad22ed848b07c0c7cb16259021cf0f175b
Jonatan Villarroel <lml@live.cl>
2022-07-12 21:00:55 -0400

    Modulo 3 JS

---
+++ DL/Clases/Modulo 3/Clase 1/codigo.js
@@ -0,0 +1,11 @@
+// nombre = prompt('ingrese su nombre')
+// alert(`Hola ${nombre}`)
+// ----------------------------------------
+// num1 = prompt('numero 1')
+// num2 = prompt('numero 2')
+
+// result = Number(num1) + Number(num2)
+
+// alert(`El resultado es ${ressult}`)
+// ----------------------------------------
+
